3c56b38cc7db8ccfc74b15b8de7c34a7728028e4
roblebob <robert.koppisch@gmail.com>
2024-04-22 16:24:30 +0200

    iteration 1-3

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,70 @@ import studentsData from "./assets/students.json";
 function App() {
   const [students, setStudents] = useState(studentsData);
 
+  const [formName, setFormName] = useState("");
+  const [formImage, setFormImage] = useState("");
+  const [formPhone, setFormPhone] = useState("");
+  const [formEmail, setFormEmail] = useState("");
+  const [formProgram, setFormProgram] = useState("-- None --");
+  const [formGraduationYear, setFormGraduationYear] = useState(0);
+  const [formGraduated, setFormGraduated] = useState(false);
+
+  const [password, setPassword] = useState("");
+
+  const handleFormName = (e) => {
+    console.log(e.target.value);
+    setFormName(e.target.value);
+  };
+
+  const handleFormImage = (e) => {
+    setFormImage(e.target.value);
+  };
+
+  const handleFormPhone = (e) => {
+    setFormPhone(e.target.value);
+  };
+
+  const handleFormEmail = (e) => {
+    setFormEmail(e.target.value);
+  };
+
+  const handleFormProgram = (e) => {
+    setFormProgram(e.target.value);
+  };
+
+  const handleFormGraduationYear = (e) => {
+    setFormGraduationYear(e.target.value);
+  };
+
+  const handleFormGraduated = (e) => {
+    setFormGraduated(e.target.checked);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const newStudent = {
+      fullName: formName,
+      image: formImage,
+      phone: formPhone,
+      email: formEmail,
+      program: formProgram,
+      graduationYear: formGraduationYear,
+      graduated: formGraduated,
+    };
+
+    const newStudents = [...students, newStudent];
+    setStudents(newStudents);
+
+    // reset form
+    setFormName("");
+    setFormImage("");
+    setFormPhone("");
+    setFormEmail("");
+    setFormProgram("-- None --");
+    setFormGraduationYear(0);
+    setFormGraduated(false);
+  };
 
   return (
     <div className="App pt-20">
@@ -20,29 +84,49 @@ function App() {
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input
+              name="fullName"
+              type="text"
+              placeholder="Full Name"
+              onChange={handleFormName}
+            />
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input
+              name="image"
+              type="url"
+              placeholder="Profile Image"
+              onChange={handleFormImage}
+            />
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input
+              name="phone"
+              type="tel"
+              placeholder="Phone"
+              onChange={handleFormPhone}
+            />
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              onChange={handleFormEmail}
+            />
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select name="program" onChange={handleFormProgram}>
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -60,24 +144,26 @@ function App() {
               maxLength={4}
               min={2023}
               max={2030}
+              onChange={handleFormGraduationYear}
             />
           </label>
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input
+              name="graduated"
+              type="checkbox"
+              onChange={handleFormGraduated}
+            />
           </label>
 
-          <button type="submit">Add Student</button>
+          <button type="submit" onClick={handleFormSubmit}>Add Student</button>
         </div>
-
       </form>
       {/* FORM END */}
 
-
       {/* TABLE/LIST HEADER */}
       <TableHeader />
-
 
       {/* STUDENT LIST */}
       {students &&
